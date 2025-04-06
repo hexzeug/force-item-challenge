77905a712bb88e25627d327dd0ded96ee0df1f83
Siemen Zielke <77232580+hexzeug@users.noreply.github.com>
2025-04-06 19:51:03 +0200

    initial commit

---
+++ src/client/java/com/hexzeug/forceitemchallenge/ForceItemChallengeClient.java
@@ -0,0 +1,10 @@
+package com.hexzeug.forceitemchallenge;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ForceItemChallengeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
